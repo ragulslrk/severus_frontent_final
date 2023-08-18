c60beb86bb2fdf389f03d35b147a7d19da043b30
ragulslrk <slrk4444@gmail.com>
2023-08-18 19:27:23 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -5,10 +5,7 @@ import { Inter } from 'next/font/google'
 import { StyledEngineProvider } from "@mui/material/styles";
 const inter = Inter({ subsets: ['latin'] })
 import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
-export const metadata = {
-  title: 'Severus Ecommerce',
-  description: 'This is severus Ecommerce.',
-}
+
 const queryClient = new QueryClient()
 
 export default function RootLayout({ children }) {
